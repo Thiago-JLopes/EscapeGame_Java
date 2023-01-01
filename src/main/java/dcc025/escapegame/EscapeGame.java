31be7a22ac128041375bd85d6f4ded3f5da57d5a
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2023-01-01 15:25:07 -0300

    Cria tela inicial de boas vindas e seleção modo de jogo.

--- src/main/java/dcc025/escapegame/EscapeGame.java
+++ src/main/java/dcc025/escapegame/EscapeGame.java
@@ -1,16 +1,28 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package dcc025.escapegame;
 
 /**
  *
  * @author thiago
  */
+import java.util.*;
+
 public class EscapeGame {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("################################################");
+        System.out.println("                 ESCAPE GAME");
+        System.out.println("################################################");
+//-------------------------------------------------------------------------------
+        Scanner teclado = new Scanner(System.in);
+        String opcao = "0";
+        
+        System.out.println("Bem vindo ao Escape Game!");
+        System.out.println("Escolha o nível de dificuldade: ");
+        System.out.println("0 - Fácil\n1 - Médio\n2 - Díficil");
+        
+        opcao = teclado.next();
     }
 }
