07dfd29597002d5029e7eb6817ff124bc52d585f
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2023-01-01 10:50:09 -0300

    Criar arquivo inicial, configura pom.xml.

---
+++ src/main/java/dcc025/escapegame/EscapeGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dcc025.escapegame;
+
+/**
+ *
+ * @author thiago
+ */
+public class EscapeGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
