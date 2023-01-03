6d416a76fb7425f037e40c2340497c967f60f329
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2023-01-03 17:24:22 -0300

    Cria método para inicializzar o tabuleiro com a saída e as bombas.

--- src/main/java/dcc025/escapegame/Tabuleiro.java
+++ src/main/java/dcc025/escapegame/Tabuleiro.java
@@ -4,31 +4,58 @@
  */
 package dcc025.escapegame;
 
-import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Pattern;
+import java.util. *;
 
 /**
  *
  * @author thiago
  */
-public abstract class Tabuleiro implements Sorteador {
+public abstract class Tabuleiro {
 
-    static String tabuleiro[][] = new String[10][10];
-    static List<String> jogadasRealizadas;
+    String tabuleiro[][];
+    List<String> jogadasRealizadas;
+    Random numAleatorio = new Random();
     int numBombas;
-
-    public Tabuleiro(){
+    int linha;
+    int coluna;
+    
+    public int sortearPosicoes(int min, int max) {
+        return this.numAleatorio.nextInt(min, max);
     }
-
+    
+    public void InicializarTabuleiro() {
+        //Inicializa o tabuleiro
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; i++) {
+                this.tabuleiro[i][j] = " ";
+            }
+        }
+        this.tabuleiro[0][0] = " P ";
+        this.linha = sortearPosicoes(1, 10);
+        this.coluna = sortearPosicoes(0,10);
+        this.tabuleiro[linha][coluna] = "S";
+        //----------------------------------------------------------------------
+        //Adiciona as bombas
+        int cont = 0;
+        do {
+            this.linha = sortearPosicoes(0, 10);
+            this.coluna = sortearPosicoes(0, 10);
+            
+            if(this.tabuleiro[this.linha][this.coluna].equals(" ")) {
+                this.tabuleiro[this.linha][this.coluna] = "B";
+                cont++;
+            }
+        } while(cont < this.numBombas);
+    }
+    
     public String[] leJogada() throws OpcaoException {
         Scanner teclado = new Scanner(System.in);
-        
+         
         System.out.println("Digite a sua jogada no formato (direção, deslocamento): ");
         String leituraJogada = teclado.nextLine();
         String Jogada[] = leituraJogada.substring(1, 5).split(", ");
         
-        if(!Jogada[0].equals("d") || !Jogada[0].equals("e") || !Jogada[0].equals("c") || !Jogada[0].equals("c") || !Jogada[0].equals("b")) {
+        if(Jogada[0].equals("d") || !Jogada[0].equals("e") || !Jogada[0].equals("c") || !Jogada[0].equals("c") || !Jogada[0].equals("b")) {
             throw new OpcaoException();
         }
         int dir = Integer.parseInt(Jogada[1]);
