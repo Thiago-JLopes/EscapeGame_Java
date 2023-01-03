6e5cd5e813bba3747ec4777f900cfc64c4167ffc
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2023-01-02 22:43:15 -0300

    Adiciona leitura da opção e chama a função para leitura da jogada.

--- src/main/java/dcc025/escapegame/EscapeGame.java
+++ src/main/java/dcc025/escapegame/EscapeGame.java
@@ -11,18 +11,21 @@ import java.util.*;
 
 public class EscapeGame {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws OpcaoException {
         System.out.println("################################################");
         System.out.println("                 ESCAPE GAME");
         System.out.println("################################################");
 //-------------------------------------------------------------------------------
         Scanner teclado = new Scanner(System.in);
-        String opcao = "0";
-        
+        String opcaoModoJogo = "0";
+
         System.out.println("Bem vindo ao Escape Game!");
         System.out.println("Escolha o nível de dificuldade: ");
         System.out.println("0 - Fácil\n1 - Médio\n2 - Díficil");
         
-        opcao = teclado.next();
+        Tabuleiro novoTabuleiro = new TabuleiroFacil();
+        
+        novoTabuleiro.leJogada();
+
     }
 }
