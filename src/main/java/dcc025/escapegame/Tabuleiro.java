46b2432d09f57fccfc4ac1ecd136f9ebd00129bf
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2023-01-04 17:00:11 -0300

    Adiciona método de imprimir e altera atributos para static e cria métodos de ascesso.

--- src/main/java/dcc025/escapegame/Tabuleiro.java
+++ src/main/java/dcc025/escapegame/Tabuleiro.java
@@ -4,7 +4,7 @@
  */
 package dcc025.escapegame;
 
-import java.util. *;
+import java.util.*;
 
 /**
  *
@@ -12,62 +12,117 @@ import java.util. *;
  */
 public abstract class Tabuleiro {
 
-    String tabuleiro[][];
+    private static String tabuleiro[][] = new String[10][10];
     List<String> jogadasRealizadas;
     Random numAleatorio = new Random();
     int numBombas;
-    int linha;
-    int coluna;
-    
+    private static int pX;
+    private static int pY;
+    private static int linha;
+    private static int coluna;
+
+    public static int getpX() {
+        return pX;
+    }
+
+    public static void setpX(int pX) {
+        Tabuleiro.pX = pX;
+    }
+
+    public static int getpY() {
+        return pY;
+    }
+
+    public static void setpY(int pY) {
+        Tabuleiro.pY = pY;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public void setLinha(int linha) {
+        Tabuleiro.linha = linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(int coluna) {
+        Tabuleiro.coluna = coluna;
+    }
+
     public int sortearPosicoes(int min, int max) {
         return this.numAleatorio.nextInt(min, max);
     }
-    
+
     public void InicializarTabuleiro() {
         //Inicializa o tabuleiro
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 10; i++) {
-                this.tabuleiro[i][j] = " ";
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                Tabuleiro.tabuleiro[i][j] = "   ";
             }
         }
-        this.tabuleiro[0][0] = " P ";
-        this.linha = sortearPosicoes(1, 10);
-        this.coluna = sortearPosicoes(0,10);
-        this.tabuleiro[linha][coluna] = "S";
+        Tabuleiro.tabuleiro[0][0] = " P ";
+        //localização do P
+        setpX(0);
+        setpY(0);
+        Tabuleiro.linha = sortearPosicoes(1, 10);
+        Tabuleiro.coluna = sortearPosicoes(0, 10);
+        Tabuleiro.tabuleiro[getLinha()][getColuna()] = " S ";
         //----------------------------------------------------------------------
         //Adiciona as bombas
         int cont = 0;
         do {
-            this.linha = sortearPosicoes(0, 10);
-            this.coluna = sortearPosicoes(0, 10);
-            
-            if(this.tabuleiro[this.linha][this.coluna].equals(" ")) {
-                this.tabuleiro[this.linha][this.coluna] = "B";
+            setLinha(sortearPosicoes(0, 10));
+            setColuna(sortearPosicoes(0, 10));
+
+            if (this.tabuleiro[getLinha()][getColuna()].equals("   ")) {
+                this.tabuleiro[linha][coluna] = " B ";
                 cont++;
             }
-        } while(cont < this.numBombas);
+
+        } while (cont < this.numBombas);
+    }
+
+    public void imprimirSemBombas() {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 10; j++) {
+                if (Tabuleiro.tabuleiro[i][j].equals(" P ")) {
+                    System.out.print(Tabuleiro.tabuleiro[i][j] + "|");
+                } else {
+                    System.out.print("   |");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public void imprimir() {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 10; j++) {
+                System.out.print(Tabuleiro.tabuleiro[i][j] + "|");
+            }
+            System.out.println("");
+        }
+    }
+
+    public void inserirJogada(String[] jogada) {
+        this.jogadasRealizadas.add("(" + jogada[0] + ", " + jogada[1] + ")");
+        String direcao = jogada[0];
+        int deslocamento = Integer.parseInt(jogada[1]);
+        int posicaoD = 0;
+        
+        
+       
+
     }
     
-    public String[] leJogada() throws OpcaoException {
-        Scanner teclado = new Scanner(System.in);
-         
-        System.out.println("Digite a sua jogada no formato (direção, deslocamento): ");
-        String leituraJogada = teclado.nextLine();
-        String Jogada[] = leituraJogada.substring(1, 5).split(", ");
-        
-        if(Jogada[0].equals("d") || !Jogada[0].equals("e") || !Jogada[0].equals("c") || !Jogada[0].equals("c") || !Jogada[0].equals("b")) {
-            throw new OpcaoException();
-        }
-        int dir = Integer.parseInt(Jogada[1]);
-        if(dir < 0 || dir > 10) {
-            throw new OpcaoException();
-        }
-        
-        return Jogada;
-    }   
 
-    public void inserirJogada() {
-        System.out.println("Jogada inserida!");
-
+    public boolean fimDejogo(String s) {
+        return s.equals(" B ") || s.equals("S");
     }
 }
