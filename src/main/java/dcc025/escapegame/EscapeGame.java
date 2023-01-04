95401fb7d0d07dacfd8ba89a4d8564c83982ed7b
Thiago Lopes <thiago.jose@estudante.ufjf.br>
2023-01-04 16:58:49 -0300

    Atualiza tratamento de exceções.

--- src/main/java/dcc025/escapegame/EscapeGame.java
+++ src/main/java/dcc025/escapegame/EscapeGame.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 public class EscapeGame {
 
-    public static void main(String[] args) throws OpcaoException {
+    public static void main(String[] args) {
         System.out.println("################################################");
         System.out.println("                 ESCAPE GAME");
         System.out.println("################################################");
@@ -23,12 +23,17 @@ public class EscapeGame {
         System.out.println("Escolha o nível de dificuldade: ");
         System.out.println("0 - Fácil\n1 - Médio\n2 - Díficil");
 
-        opcaoModoJogo = teclado.nextLine();
+        opcaoModoJogo = teclado.next();
         String jogada[] = new String[2];
         switch (opcaoModoJogo) {
             case "0":
                 novoTabuleiro = new TabuleiroFacil();
+                novoTabuleiro.InicializarTabuleiro();
+                novoTabuleiro.imprimirSemBombas();
+                System.out.println("");
+                novoTabuleiro.imprimir();
                 jogada = leJogada();
+                //novoTabuleiro.inserirJogada(jogada);
                 break;
             case "1":
                 break;
@@ -41,24 +46,23 @@ public class EscapeGame {
 
     }
 
-    public static String[] leJogada(){
+    public static String[] leJogada() {
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Digite a sua jogada no formato (direção, deslocamento): ");
         String leituraJogada = teclado.nextLine();
         String Jogada[] = leituraJogada.substring(1, leituraJogada.length() - 1).split(", ");
-        System.out.println(Jogada[1]);
 
         try {
             validarJogada(Jogada);
-        } catch (OpcaoException | NumberFormatException e) {
+        } catch (OpcaoException | NumberFormatException | ArrayIndexException e) {
             System.out.println("Direção e/ou deslocamento inválido!\nJOGUE NOVAMENTE!");
             return leJogada();
         }
         return Jogada;
     }
 
-    private static void validarJogada(String[] Jogada) throws OpcaoException {
+    private static void validarJogada(String[] Jogada) throws OpcaoException, ArrayIndexException {
         if (!Jogada[0].contains("d") && !Jogada[0].contains("e") && !Jogada[0].contains("b") && !Jogada[0].contains("c")) {
             throw new OpcaoException();
         }
@@ -72,6 +76,30 @@ public class EscapeGame {
             }
         } catch (NumberFormatException | OpcaoException e) {
             throw new NumberFormatException();
+        }
+
+        int deslocamento = Integer.parseInt(Jogada[1]);
+        switch (Jogada[0]) {
+            case "d":
+                if (Tabuleiro.getpX() + deslocamento > 9) {
+                    throw new ArrayIndexException();
+                }
+                break;
+            case "e":
+                if (Tabuleiro.getpX() - deslocamento < 0) {
+                    throw new ArrayIndexException();
+                }
+                break;
+            case "c":
+                if (Tabuleiro.getpY() - deslocamento < 0) {
+                    throw new ArrayIndexException();
+                }
+                break;
+            default:
+                if (Tabuleiro.getpY() + deslocamento > 9) {
+                    throw new ArrayIndexException();
+                }
+                break;
         }
     }
 }
